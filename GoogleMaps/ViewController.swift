92328efe552a933e36f081006cf976fa838d4b14
Jagdeep <simran6233@gmail.com>
2018-07-13 13:21:35 -0500

    Core_Location_get location

--- GoogleMaps/ViewController.swift
+++ GoogleMaps/ViewController.swift
@@ -7,14 +7,65 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,CLLocationManagerDelegate {
+   
+    var locationManager = CLLocationManager()
+    var lat: Double!
+    var long: Double!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        setUpCLLocationManager()
     }
-
-
+    func setUpCLLocationManager() {
+        // CLLocation Mananger
+        
+        
+        self.locationManager.delegate = self
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.locationManager.requestWhenInUseAuthorization()
+        self.locationManager.startUpdatingLocation()
+        self.locationManager.distanceFilter = 1000
+        
+        if CLLocationManager.authorizationStatus() == .notDetermined {
+            locationManager.requestAlwaysAuthorization()
+        }
+            // 2. authorization were denied
+        else if CLLocationManager.authorizationStatus() == .denied {
+            print("Location services were previously denied. Please enable location services for this app in Settings.")
+        }
+            // 3. we do have authorization
+        else if CLLocationManager.authorizationStatus() == .authorizedAlways {
+            locationManager.startUpdatingLocation()
+        
+        }
+       
+    }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let locValue:CLLocationCoordinate2D = manager.location!.coordinate
+        long = (locValue.longitude)
+        lat = (locValue.latitude)
+        
+        print (long , lat)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+            let locValue: CLLocationCoordinate2D = (manager.location!.coordinate)
+            print("locations = \(locValue.latitude) \(locValue.longitude)")
+            long = (locValue.longitude)
+            lat = (locValue.latitude)
+        }
+    }
+   
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error.localizedDescription)
+        self.locationManager.stopUpdatingLocation()
+    }
 }
 
